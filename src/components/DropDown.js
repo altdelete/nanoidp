d9d737250fdceca54fd186dd250bf2bff73e4eaf
Benjamin Cole <benjamin.cole@apple.com>
2023-10-19 11:16:33 -0400

    Imported useUser hook from userContext

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -1,12 +1,15 @@
 import { Fragment } from 'react';
 import { Menu, Transition } from '@headlessui/react';
 import { ChevronDownIcon } from '@heroicons/react/20/solid';
+import { useUser } from '@/contexts/UserContext';
 
 function classNames(...classes) {
 	return classes.filter(Boolean).join(' ');
 }
 
-export default function DropDown({ user }) {
+export default function DropDown() {
+	const { user } = useUser();
+
 	const logout = async () => {
 		try {
 			const res = await fetch('/api/logout', {
@@ -27,7 +30,7 @@ export default function DropDown({ user }) {
 		<Menu as="div" className="relative inline-block text-left">
 			<div>
 				<Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-					{user ? user.firstName : 'Options'}
+					{user ? user.firstName : 'Anon'}
 					<ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
 				</Menu.Button>
 			</div>
